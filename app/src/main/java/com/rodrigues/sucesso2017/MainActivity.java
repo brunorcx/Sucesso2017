0ff633ff9b1a5cae656ab20d4e015f3ac4a963b7
brunorcx <brunorcx@hotmail.com>
2017-06-23 11:08:49 -0400

    Branch firebase

--- app/src/main/java/com/rodrigues/sucesso2017/MainActivity.java
+++ app/src/main/java/com/rodrigues/sucesso2017/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.firebase.database.ValueEventListener;
 import static android.R.attr.value;
 
 public class MainActivity extends AppCompatActivity {
+  private final String TAG="Main Activity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
         // This method is called once with the initial value and again
         // whenever data at this location is updated.
         String value = dataSnapshot.getValue(String.class);
-        String TAG="1";
         Log.d(TAG, "Value is: " + value);
 
         TextView primeiraView = (TextView) findViewById(R.id.primeiraView);
